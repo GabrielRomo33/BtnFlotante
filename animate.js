a3c0c05e7b2b8ffe91c3d4afe3254bcc64f9dbed
Ing. Renato Romo <rromo@grand.com.mx>
2023-06-19 10:19:30 -0600

    se subio el repositorio

---
+++ animate.js
@@ -0,0 +1,23 @@
+
+$('.botonF1').hover(function(){
+    $('.btn').addClass('animacionVer');
+  })
+  $('.cont').mouseleave(function(){
+    $('.btn').removeClass('animacionVer');
+  })
+  $('.botonF2').click(function(){
+    $('body').css('background-color','#2196F3')
+  })
+  $('.botonF3').click(function(){
+    $('body').css('background-color','#673AB7')
+  })
+  $('.botonF4').click(function(){
+    $('body').css('background-color','#009688')
+  })
+  $('.botonF5').click(function(){
+    $('body').css('background-color','#FF5722')
+  })
+  $('.botonF1').click(function (){
+    $('body').css('background-color','#ffffff')
+  })
+ 
